600c68b8c780067fc27780ba6ab85ea533758d0b
Amanda LeMoine <amanda1@mnlemoines.com>
2021-01-04 16:59:49 -0800

    connection working

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,80 @@
+// Import MySQL connection
 const connection = require("./connection");
 
+function printQuestionMarks(num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    let arr = [];
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+      let value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+  
+
 const orm = {
-  selectAll: function () {},
-  insertOne: function () {},
-  updateOne: function () {},
+  selectAll: function (tableInput, cb) {
+    connection.query(
+      "SELECT * FROM " + tableInput + ";",
+      function (err, result) {
+        if (err) throw err;
+        cb(result);
+      }
+    );
+  },
+  insertOne: function (table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  updateOne: function () {
+    let queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
 };
 
 module.exports = orm;
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,6 +1,50 @@
 const express = require("express");
-const burger = require("../models/burger");
+const burger = require("../models/burger.js");
 
-const router = {}
+const router = express.Router();
 
+router.get("/",function(req,res){
+    burger.selectAll()
+})
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    let hbsObject = {
+      burger: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  cat.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
+// Export routes for server.js to use.
 module.exports = router;
