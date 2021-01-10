1e53fd3b64e6bf383139073a1e4c4d78ba0afef6
Amanda LeMoine <amanda1@mnlemoines.com>
2021-01-10 14:27:21 -0800

    jawsdb_url added to conneciton

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "WrexyBoi!9",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "WrexyBoi!9",
+    database: "burgers_db",
+  });
+}
 
 connection.connect(function (err) {
   if (err) throw err;
